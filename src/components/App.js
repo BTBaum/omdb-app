4073920de50f6779c88a2a429851853e7ac4a2bf
Brandon Baum <bbaum@redhat.com>
2018-07-14 19:28:32 -0400

    Added basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,16 @@ import Header from './Header'
 import SearchBar from '../containers/SearchBar'
 import Table from '../containers/Table'
 
+const styles = {
+  app: {
+    backgroundColor: "#EFF8FF"
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="container-fluid m-0 p-0" style={styles.app}>
         <Header />
         <SearchBar />
         <Table />
