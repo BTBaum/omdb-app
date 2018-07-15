764c20552e2501adb35ffbb40bb9a703ad1ec7de
Brandon Baum <bbaum@redhat.com>
2018-07-15 16:47:29 -0400

    Added comment to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+// In prod app I would use environment variables
+// process.env.REACT_API_KEY and process.env.OMDB_API
+
 const API_KEY = "9f572b90"
 const OMDB_API = `https://www.omdbapi.com/?apikey=${API_KEY}`
 
